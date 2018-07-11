89e7a4091af65eb840748ceb51fcabff151b2705
Andy Chrzaszcz <andy9775@gmail.com>
2018-07-11 17:51:23 -0400

    update key logic
    Ensure that each key returns an identifier (alias for string) rather
    than a string directly. This allows for better identification of the
    type information to be expected throughout the library

--- key.go
+++ key.go
@@ -5,16 +5,20 @@ type Key interface {
 	// String should return a guaranteed unique string that can be used to identify
 	// the element. For example in an array. Overlapping results from calls to this
 	// method will result in incorrect data being returned.
-	String() string
+	String() Identifier
 }
+
+// Identifier is an alias for a string which uniquely identifies each element to be
+// resolved
+type Identifier string
 
 // Keys wraps an array of keys and contains accessor methods
 type Keys []Key
 
 // Keys returns a slice of the unique key values by calling the `String()`
 // functions on each element
-func (keys Keys) Keys() []string {
-	result := make([]string, 0, len(keys))
+func (keys Keys) Keys() []Identifier {
+	result := make([]Identifier, 0, len(keys))
 	for _, k := range keys {
 		result = append(result, k.String())
 	}
