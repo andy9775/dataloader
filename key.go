e91ebff60c0c4f1339f43f567bf25fbf83a50f68
Andy Chrzaszcz <andy9775@gmail.com>
2018-07-10 23:21:31 -0400

    Create Key interface type
    The `Key` interface is responsible for uniquely identifying each
    element. It is useful for tracking return values to callers and for
    caching, therefore each key implementing the `Key` interface should
    return a unique value for the `String()` function

---
+++ key.go
@@ -0,0 +1,22 @@
+package dataloader
+
+// Key is interface each element identifier must implement
+type Key interface {
+	// String should return a guaranteed unique string that can be used to identify
+	// the element. For example in an array. Overlapping results from calls to this
+	// method will result in incorrect data being returned.
+	String() string
+}
+
+// Keys wraps an array of keys and contains accessor methods
+type Keys []Key
+
+// Keys returns a slice of the unique key values by calling the `String()`
+// functions on each element
+func (keys Keys) Keys() []string {
+	result := make([]string, 0, len(keys))
+	for _, k := range keys {
+		result = append(result, k.String())
+	}
+	return result
+}
