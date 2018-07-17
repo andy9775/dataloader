71a346603ce456f69689246466098108314cd66d
Andy Chrzaszcz <andy9775@gmail.com>
2018-07-16 22:38:11 -0400

    add MergeWith and GetValueForString methods to resultMap
    MergeWith is responsible for merging two results sets together.
    
    GetValueForString is a getter method which returns the same value as
    GetValue except requires a string arg, not a Key

--- result.go
+++ result.go
@@ -12,6 +12,8 @@ type ResultMap interface {
 	Length() int
 	// Keys returns a slice of all unique identifiers used in the containing map (keys)
 	Keys() []string
+	MergeWith(*ResultMap)
+	GetValueForString(string) Result
 }
 
 type resultMap struct {
@@ -53,6 +55,14 @@ func (r *resultMap) GetValue(key Key) Result {
 	return r.r[key.String()]
 }
 
+func (r *resultMap) GetValueForString(key string) Result {
+	r.m.RLock()
+	defer r.m.RUnlock()
+
+	// No need to check ok, missing value from map[Any]interface{} is nil by default.
+	return r.r[key]
+}
+
 func (r *resultMap) Keys() []string {
 	r.m.RLock()
 	defer r.m.RUnlock()
@@ -69,4 +79,13 @@ func (r *resultMap) Length() int {
 	defer r.m.RUnlock()
 
 	return len(r.r)
+}
+
+func (r *resultMap) MergeWith(m *ResultMap) {
+	r.m.Lock()
+	defer r.m.Unlock()
+
+	for _, k := range (*m).Keys() {
+		r.r[k] = (*m).GetValueForString(k)
+	}
 }
