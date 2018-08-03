7cc99965423c8d69825bd0896a114dd56af5fd44
Andy Chrzaszcz <andy9775@gmail.com>
2018-08-02 21:45:23 -0400

    Remove mutex from counter
    Counter will no longer be updated from multiple go routines hence there
    is no need to lock on updates/reads to the counter.

--- strategies/counter.go
+++ strategies/counter.go
@@ -1,7 +1,5 @@
 package strategies
 
-import "sync"
-
 // Counter provides the interface to a Load call counter.
 // A Load call counter provides helper methods to track the
 // number of increments and identify when the increments equal
@@ -17,29 +15,21 @@ type Counter interface {
 // NewCounter returns a new instance of a Load call counter
 func NewCounter(capacity int) Counter {
 	return &counter{
-		capacity:     capacity,
-		loadCalls:    0,
-		counterMutex: &sync.Mutex{},
+		capacity:  capacity,
+		loadCalls: 0,
 	}
 }
 
 type counter struct {
-	counterMutex *sync.Mutex
-	loadCalls    int
-	capacity     int
+	loadCalls int
+	capacity  int
 }
 
 func (c *counter) Increment() bool {
-	c.counterMutex.Lock()
-	defer c.counterMutex.Unlock()
-
 	c.loadCalls++
 	return c.loadCalls >= c.capacity
 }
 
 func (c *counter) ResetCount() {
-	c.counterMutex.Lock()
-	defer c.counterMutex.Unlock()
-
 	c.loadCalls = 0
 }
